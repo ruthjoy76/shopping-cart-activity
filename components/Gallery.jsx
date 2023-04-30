5bb5829dff05432eda1e789304d0572baf47ffa7
ruthjoy76 <ruthjoy76@gmail.com>
2023-04-30 10:05:59 +0800

    first commit

---
+++ components/Gallery.jsx
@@ -0,0 +1,33 @@
+import "./Gallery.css";
+import { useState, useEffect } from "react";
+
+
+function Gallery() {
+  const [products, setProducts] = useState([]);
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const data = await response.json();
+      setProducts(data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="card-container">
+      {products.map((product) => (
+        <div key={product.id} className="card">
+          <img src={product.image} alt={product.title} />
+          <h3>{product.title}</h3>
+          <p>{product.description}</p>
+          <p>${product.price}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+
+export default Gallery
